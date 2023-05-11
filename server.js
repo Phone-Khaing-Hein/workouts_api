67ea52e45ac06a97e44926dde5caa34846b49ca3
PhoneKhaingHein <pkh2662003@gmail.com>
2023-05-11 21:16:23 +0630

    version 1

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const workoutRoutes = require("./routes/workouts");
 const userRoutes = require("./routes/users");
+const cors = require("cors");
 
 const app = express();
 
